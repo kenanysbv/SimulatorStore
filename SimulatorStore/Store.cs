3f3182a33e735caf31098badb2e768d7ed32b5ba
kenan <86625368+kenanysbv@users.noreply.github.com>
2022-05-15 01:29:40 +0400

    Added Store And WorkerController basic

---
+++ SimulatorStore/Store.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SSModels;
+using SSExceptions;
+using SSGeneral;
+
+namespace SS
+{
+    public class Store
+    {
+        public Store() { }
+
+
+
+        public List<Aisle> Aisles { get; set; }
+
+
+
+        public void AddAisle(Aisle aisle) => Aisles.Add(aisle);
+
+        public void RemoveAisle(Aisle aisle) => Aisles.Remove(aisle);
+    }
+}
---
+++ SimulatorStore/WorkerController.cs
@@ -0,0 +1,30 @@
+﻿using SSModels;
+using SSGeneral;
+using SSExceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SS
+{
+    public class WorkerController
+    {
+        public WorkerController() { }
+
+        public WorkerController(List<Worker> workers) => Workers = workers;
+
+
+
+        public List<Worker> Workers { get; set; }
+
+
+
+        public void Check() => Workers.ForEach(w => w.Check());
+
+        public void Preapere() => Workers.ForEach(w => w.Preapera());
+
+        public void CheckForNewWorkers() { }
+    }
+}
